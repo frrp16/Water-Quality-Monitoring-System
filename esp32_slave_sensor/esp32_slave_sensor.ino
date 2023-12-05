be4df29d45cd4037e1215cc2a33c58a58bfffe60
zulfikar <zulfikar.hadzalic@ui.ac.id>
2023-12-05 14:36:18 +0700

    sensor with real tds&temp sensors

--- esp32_slave_sensor/esp32_slave_sensor.ino
+++ esp32_slave_sensor/esp32_slave_sensor.ino
@@ -1,12 +1,42 @@
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 // WiFi and MQTT constants
-#define WIFI_SSID "Wokwi-GUEST"
-#define WIFI_PASSWORD ""
+#define WIFI_SSID "Galaxy A7153E8"
+#define WIFI_PASSWORD "lemaopantek"
 #define MQTT_BROKER "broker.hivemq.com" // Broker Testing: https://www.hivemq.com/demos/websocket-client/
 #define MQTT_PORT 8883
 #define MQTT_TOPIC "FinalProject/B7/water_quality"
+
+#define TdsSensorPin 27
+#define VREF 3.3              // analog reference voltage(Volt) of the ADC
+#define SCOUNT  30            // sum of sample point
+
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 4;
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor
+DallasTemperature sensors(&oneWire);
+
+// Speaker connected to GPIO pin 12
+int speakerPin = 12;
+
+int analogBuffer[SCOUNT];     // store the analog value in the array, read from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0;
+int copyIndex = 0;
+
+float averageVoltage = 0;
+float tdsValue = 0;
+float temperatureC = 25;  
+float temperature = 25;       // current temperature for compensation
 
 // Certificates
 const char* server_cert = R"(-----BEGIN CERTIFICATE-----
@@ -102,12 +132,12 @@ void wifiTask(void* pvParameters);
 bool mqttConnect();
 float generateRandomNumber(float min, float max);
 
-void getTemperature();
-void getTDS();
+void getTemperature(void* pvParameters);
+void getTDS(void* pvParameters);
 void getTurbidity();
 
 
-float temperature = 25, tds = 0, turbidity = 0;
+float turbidity = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -123,6 +153,14 @@ void setup() {
 
   // start wifi connection task
   xTaskCreatePinnedToCore(wifiTask, "Wifi", 4096, NULL, 1, NULL, 1);
+  // Create the temperature sensing task
+  xTaskCreate(getTemperature, "TemperatureTask", 4096, NULL, 1, NULL);
+
+  pinMode(TdsSensorPin, INPUT);
+  pinMode(speakerPin, OUTPUT);
+
+  // Create the TDS sensing task
+  xTaskCreate(getTDS, "TdsTask", 4096, NULL, 1, NULL);
 }
 
 void loop() {
@@ -192,10 +230,8 @@ bool mqttConnect() {
 
 // callback for publish data to MQTT topic every 5 seconds
 void sendDataTimerCallback(TimerHandle_t xTimer) {
-  getTemperature();
-  getTDS();
   getTurbidity();
-  String message = "{\"temperature\":" + String(temperature) + ",\"tds\":" + String(tds) + ",\"turbidity\":" + String(turbidity) + "}";
+  String message = "{\"temperature\":" + String(temperatureC) + ",\"tds\":" + String(tdsValue) + ",\"turbidity\":" + String(turbidity) + "}";
   Serial.println(message);
   mqttClient.publish(MQTT_TOPIC, message.c_str());
 }
@@ -215,14 +251,94 @@ float generateRandomNumber(float min, float max) {
   return min + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(max-min)));
 }
 
-void getTemperature() {
-  // replace with real sensor reading logic!
-  temperature = generateRandomNumber(16, 50);
+void getTemperature(void* pvParameters) {
+
+ for (;;) {
+   sensors.requestTemperatures();
+   temperatureC = sensors.getTempCByIndex(0);
+   Serial.print(temperatureC);
+   Serial.println("ºC");
+
+   // If the temperature is equal to or greater than 40 degrees Celsius, the buzzer beeps
+   if (temperatureC >= 40) {
+     tone(speakerPin, 500); // Change 1000 to a higher frequency if needed
+     delay(1000); // Maintain the tone for 1 second
+     noTone(speakerPin); // Turn off the tone
+     delay(1000); // Pause for 1 second before generating the tone again
+   }
+
+   vTaskDelay(pdMS_TO_TICKS(1000)); // Delay using FreeRTOS function
+ }
+}
+// median filtering algorithm
+int getMedianNum(int bArray[], int iFilterLen) {
+  int bTab[iFilterLen];
+  for (byte i = 0; i < iFilterLen; i++)
+    bTab[i] = bArray[i];
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++) {
+    for (i = 0; i < iFilterLen - j - 1; i++) {
+      if (bTab[i] > bTab[i + 1]) {
+        bTemp = bTab[i];
+        bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+      }
+    }
+  }
+  if ((iFilterLen & 1) > 0) {
+    bTemp = bTab[(iFilterLen - 1) / 2];
+  } else {
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+  }
+  return bTemp;
 }
 
-void getTDS() {
-  // replace with real sensor reading logic!
-  tds = generateRandomNumber(0, 100);
+void getTDS(void* pvParameters) {
+  for (;;) {
+    static unsigned long analogSampleTimepoint = millis();
+    if (millis() - analogSampleTimepoint > 40U) { // every 40 milliseconds, read the analog value from the ADC
+      analogSampleTimepoint = millis();
+      analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin); // read the analog value and store into the buffer
+      analogBufferIndex++;
+      if (analogBufferIndex == SCOUNT) {
+        analogBufferIndex = 0;
+      }
+    }
+
+    static unsigned long printTimepoint = millis();
+    if (millis() - printTimepoint > 800U) {
+      printTimepoint = millis();
+      for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++) {
+        analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+
+        // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+        averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREF / 4096.0;
+
+        // temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+        float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);
+        // temperature compensation
+        float compensationVoltage = averageVoltage / compensationCoefficient;
+
+        // convert voltage value to tds value
+        tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * 0.5;
+
+        // Serial.print("voltage:");
+        // Serial.print(averageVoltage, 2);
+        // Serial.print("V   ");
+        Serial.print("TDS Value:");
+        Serial.print(tdsValue, 0);
+        Serial.println("ppm");
+      }
+    }
+
+    if (tdsValue >= 300) {
+      tone(speakerPin, 500); // Change 1000 to a higher frequency if needed
+      delay(1000); // Maintain the tone for 1 second
+      noTone(speakerPin); // Turn off the tone
+      delay(1000); // Pause for 1 second before generating the tone again
+    }
+    vTaskDelay(pdMS_TO_TICKS(10)); // adjust delay as needed for task timing
+  }
 }
 
 void getTurbidity() {
